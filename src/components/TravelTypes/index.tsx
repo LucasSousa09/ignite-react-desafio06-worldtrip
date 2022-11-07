a9ab791b555840d89a61ff3d61e8cc573db97896
LucasSousa09 <lucasrodrigues.sousa09@gmail.com>
2022-11-07 13:40:27 -0300

    layout and grammar fixes

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -6,17 +6,18 @@ import { TravelDots } from "../TravelDots";
 export function TravelTypes(){
     const [isSmallerThan720] = useMediaQuery('(max-width: 768px)')
     return (
-        <SimpleGrid columns={[1,1,5]} w='100%' maxW='1160px' gap='6rem' px='2rem'>
+        <SimpleGrid columns={[1,2,5]} w='100%' justifyItems='center' maxW='1160px' py={['2rem']} gap={['1.5rem','1.5rem','6rem']} px='2rem'>
             {
                 isSmallerThan720 ?
                 (           
-                    <SimpleGrid w='100%' pl='3rem' columns={2} rowGap='1.75rem' my='2.25rem'>
-                        <TravelDots title='vida noturna' />
+                    <>
                         <TravelDots title='praia' />
+                        <TravelDots title='vida noturna' />
                         <TravelDots title='moderno' />
                         <TravelDots title='clássico' />
                         <TravelDots title='e mais...' />
-                    </SimpleGrid>             
+                    </>
+            
                 )
                 :
                 (
